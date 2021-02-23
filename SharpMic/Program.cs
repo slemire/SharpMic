76532e7be3d030ef092afc162a2699f95a32e41e
snowscan <lemire.simon@gmail.com>
2021-02-22 22:08:56 -0500

    First commit

---
+++ SharpMic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SharpMic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2) {
+                Console.WriteLine(@"[!] Usage: SharpMic.exe <output_file (.wav)> <duration (sec)>");
+                return;
+            }
+
+            int duration = int.Parse(args[1]) * 1000;
+
+            Recorder myRecorder = new Recorder();            
+            myRecorder.StartRecording(args[0]);
+            Console.WriteLine("[+] Waiting " + args[1] + " seconds...");
+            Thread.Sleep(duration);
+            myRecorder.StopRecording();
+        }
+    }
+}
---
+++ SharpMic/Recorder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace SharpMic
+{
+    class Recorder
+    {
+        public WaveInEvent waveSource = null;
+        public WaveFileWriter waveFile = null;
+        public void StartRecording(String fileName)
+        {
+            Console.WriteLine(@"[+] Recording started");
+            waveSource = new WaveInEvent();
+            waveSource.WaveFormat = new WaveFormat(44100, 1);
+
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+            waveFile = new WaveFileWriter(fileName, waveSource.WaveFormat);
+
+            waveSource.StartRecording();
+        }
+        public void StopRecording()
+        {
+            Console.WriteLine(@"[+] Recording stopped");
+            waveSource.StopRecording();
+        }
+        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveSource != null)
+            {
+                waveSource.Dispose();
+                waveSource = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }            
+        }
+    }
+}
